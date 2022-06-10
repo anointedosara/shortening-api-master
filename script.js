da07bc9bec889c60cf629f5a5183546f9e6f4d43
anointedosara <101458383+anointedosara@users.noreply.github.com>
2022-06-10 16:19:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const shorten = async(input) => {
+    try {
+        const data = await axios.get(`https://api.shrtco.de/v2/shorten?url=${input}`);
+        console.log(data.data)
+        let result = document.getElementById("result");
+        let output = document.getElementById("output");
+        result.innerHTML = `Shortened Link: <a href="${data.data.result.full_short_link}" class="a" target="_blank">${data.data.result.full_short_link}</a>`;
+        output.innerHTML = `${input}`;
+        document.querySelector(".wrap").classList.add("show")
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+let input = document.getElementById("input");
+let submit = document.querySelector(".div7")
+
+submit.addEventListener("submit", (e) => {
+    e.preventDefault()
+    shorten(input.value)
+});
+
+let display = document.querySelector(".drop");
+let down = document.querySelector(".down");
+display.addEventListener("click", () => {
+    down.classList.toggle("display");
+    document.getElementById("body").classList.toggle("body")
+})
